9159437fe08f44a0d773ee10e506c2cd04672cae
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-07-07 20:42:18 +0530

    Refractor: Defining book price globally

--- src/main/java/com/sdb/tdd/CalculateDiscount.java
+++ src/main/java/com/sdb/tdd/CalculateDiscount.java
@@ -2,9 +2,10 @@ package com.sdb.tdd;
 
 public class CalculateDiscount {
 	
+	public static final double priceForOneBook = 50.00;
+	
 	public double getTotalPrice() {
 		double amtAfterDis;
-		double priceForOneBook = 50.00;
 		amtAfterDis = priceForOneBook;
 		return amtAfterDis;
 	}
